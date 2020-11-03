c49979fd3aa115791a26142782b1eb8de4de273d
Oscar Figueroa <Osc.Fig96@gmail.com>
2020-11-03 13:07:37 -0800

    Finished Task 1d

--- index.js
+++ index.js
@@ -58,9 +58,10 @@ Do the following:
    3. Multiply a and b and return the answer
 */
 
-function multiply(/*add your code here*/){
-    /*add your code here*/
+function multiply(a, b){
+    return a * b;
   }
+  multiply(3,3) 
 
 
 
